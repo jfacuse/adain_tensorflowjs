e200ff03d51374285ef09a51b8d1af8b43d5eae4
ÏnfiniteDreamX <jiperez11@uc.cl>
2019-10-27 21:23:58 -0400

    added decoder

--- main.js
+++ main.js
@@ -124,7 +124,6 @@ const conv2d_4_1 = tf.layers.conv2d({
 });
 
 let output = preprocess.apply(inputEncoder);
-console.log(output);
 output = conv2d_1_1.apply(output);
 output = conv2d_1_2.apply(output);
 output = maxPooling_1.apply(output);
@@ -143,7 +142,114 @@ const encoder = tf.model({
     outputs: output
 });
 
+// Ahora viene el DECODER
 
+const inputDecoder = tf.input({shape: [null, null, 512]});
+
+const deConv2d_4_1 = tf.layers.conv2d({
+  filters: 256,
+  kernelSize: 3,
+  activation: 'relu',
+  padding: 'valid',
+  name: 'deConv4_1',
+  trainable: false
+});
+const upSampling3 = tf.layers.upSampling2d({
+    name: 'upSampling3',
+    trainable: false
+});
+const deConv2d_3_4 = tf.layers.conv2d({
+    filters: 256,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv3_4',
+    trainable: false
+});
+const deConv2d_3_3 = tf.layers.conv2d({
+    filters: 256,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv3_3',
+    trainable: false
+});
+const deConv2d_3_2 = tf.layers.conv2d({
+    filters: 256,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv3_2',
+    trainable: false
+});
+const deConv2d_3_1 = tf.layers.conv2d({
+    filters: 128,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv3_1',
+    trainable: false
+});
+const upSampling2 = tf.layers.upSampling2d({
+    name: 'upSampling2',
+    trainable: false,
+});
+const deConv2d_2_2 = tf.layers.conv2d({
+    filters: 128,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv2_2',
+    trainable: false
+});
+const deConv2d_2_1 = tf.layers.conv2d({
+    filters: 64,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv2_1',
+    trainable: false
+});
+const upSampling1 = tf.layers.upSampling2d({
+    name: 'upSampling1',
+    trainable: false,
+});
+const deConv2d_1_2 = tf.layers.conv2d({
+    filters: 64,
+    kernelSize: 3,
+    activation: 'relu',
+    padding: 'valid',
+    name: 'deConv1_2',
+    trainable: false
+});
+const deConv2d_1_1 = tf.layers.conv2d({
+    filters: 3,
+    kernelSize: 3,
+    padding: 'valid',
+    name: 'deConv1_1',
+    trainable: false
+});
+
+
+let outputDecoder = deConv2d_4_1.apply(inputDecoder);
+outputDecoder = upSampling3.apply(outputDecoder);
+outputDecoder = deConv2d_3_4.apply(outputDecoder);
+outputDecoder = deConv2d_3_3.apply(outputDecoder);
+outputDecoder = deConv2d_3_2.apply(outputDecoder);
+outputDecoder = deConv2d_3_1.apply(outputDecoder);
+outputDecoder = upSampling2.apply(outputDecoder);
+outputDecoder = deConv2d_2_2.apply(outputDecoder);
+outputDecoder = deConv2d_2_1.apply(outputDecoder);
+outputDecoder = upSampling1.apply(outputDecoder);
+outputDecoder = deConv2d_1_2.apply(outputDecoder);
+outputDecoder = deConv2d_1_1.apply(outputDecoder);
+
+const decoder = tf.model({
+    inputs: inputDecoder,
+    outputs: outputDecoder
+});
+
+console.log(decoder);
 
 
 
